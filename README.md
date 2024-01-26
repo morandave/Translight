978eb5bdf0cdc45c1d25bf7f60ba080fc221f868
morandave <100688146+morandave@users.noreply.github.com>
2024-01-26 09:28:57 +0800

    初始化

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
+<<<<<<< HEAD
 # Translight
 一个多种语言的文物展示app
+=======
+# best_flutter_ui_templates
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+>>>>>>> 71589a1 (feat:文物列表)
---
+++ lib/model/homelist.dart
@@ -0,0 +1,34 @@
+import 'package:best_flutter_ui_templates/design_course/home_design_course.dart';
+import 'package:best_flutter_ui_templates/fitness_app/fitness_app_home_screen.dart';
+import 'package:best_flutter_ui_templates/hotel_booking/hotel_home_screen.dart';
+import 'package:best_flutter_ui_templates/introduction_animation/introduction_animation_screen.dart';
+import 'package:flutter/widgets.dart';
+
+class HomeList {
+  HomeList({
+    this.navigateScreen,
+    this.imagePath = '',
+  });
+
+  Widget? navigateScreen;
+  String imagePath;
+
+  static List<HomeList> homeList = [
+    HomeList(
+      imagePath: 'assets/introduction_animation/NanJingMuseum.jpg',
+      navigateScreen: IntroductionAnimationScreen(),
+    ),
+    HomeList(
+      imagePath: 'assets/hotel/XuZhouMuseum.jpg',
+      navigateScreen: HotelHomeScreen(),
+    ),
+    HomeList(
+      imagePath: 'assets/fitness_app/GuGongMuseum.png',
+      navigateScreen: FitnessAppHomeScreen(),
+    ),
+    HomeList(
+      imagePath: 'assets/design_course/design_course.png',
+      navigateScreen: DesignCourseHomeScreen(),
+    ),
+  ];
+}
